9f2b89a077c7c9113d7ab457224feffed70694f4
Banduty <bandutysmods@gmail.com>
2025-06-01 18:47:57 +0200

    version 1.2.4

--- src/main/java/banduty/ticktweaks/mixin/ItemEntityMixin.java
+++ src/main/java/banduty/ticktweaks/mixin/ItemEntityMixin.java
@@ -2,18 +2,10 @@ package banduty.ticktweaks.mixin;
 
 import banduty.ticktweaks.TickTweaks;
 import banduty.ticktweaks.util.TickHandlerUtil;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.network.PacketByteBuf;
-//? if >= 1.20.5 {
-    import net.minecraft.network.packet.CustomPayload;
-    import net.minecraft.network.packet.s2c.common.CustomPayloadS2CPacket;
-//?}
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -29,21 +21,12 @@ import java.util.WeakHashMap;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin {
-
     @Unique
     private static final Map<ItemEntity, Integer> TICK_TIME_MAP = Collections.synchronizedMap(new WeakHashMap<>());
 
     @Unique
     double itemDetectionRange = TickTweaks.CONFIG.entityTickSettings.itemEntities.getDetectionRange();
 
-
-    //? if >= 1.20.5 {
-    @Unique
-    private static final CustomPayload.Id<? extends CustomPayload> DUMMY_SYNC_PACKET = new CustomPayload.Id<>(Identifier.of(TickTweaks.MOD_ID, "dummy_sync"));
-     //?} else if >= 1.19.3 && <= 1.20.4 {
-    /*@Unique
-    private static final Identifier DUMMY_SYNC_PACKET = new Identifier(TickTweaks.MOD_ID, "dummy_sync");
-     *///?}
 
     @ModifyArgs(
             method = "tryMerge()V",
@@ -60,37 +43,15 @@ public abstract class ItemEntityMixin {
     private void onTickEntity(CallbackInfo ci) {
         ItemEntity itemEntity = (ItemEntity) (Object) this;
         World world = itemEntity.getWorld();
-        if (!(world instanceof ServerWorld serverWorld)) return;
+        if (world instanceof ServerWorld serverWorld) {
+            MinecraftServer server = serverWorld.getServer();
+            int currentTickTime = TICK_TIME_MAP.getOrDefault(itemEntity, 0);
 
-        MinecraftServer server = serverWorld.getServer();
-        int currentTickTime = TICK_TIME_MAP.getOrDefault(itemEntity, 0);
+            if (!TickTweaks.CONFIG.entityTickSettings.itemEntities.enabled || TickHandlerUtil.tickCancellation(server, ci, true,
+                    TickTweaks.CONFIG.entityTickSettings.itemEntities.getFixedTickRate(), currentTickTime, 0))
+                TICK_TIME_MAP.put(itemEntity, 0);
 
-        if (!TickTweaks.CONFIG.entityTickSettings.itemEntities.enabled || TickHandlerUtil.tickCancellation(server, ci, true,
-                TickTweaks.CONFIG.entityTickSettings.itemEntities.getFixedTickRate(), currentTickTime, 0))
-            TICK_TIME_MAP.put(itemEntity, 0);
-
-        TICK_TIME_MAP.put(itemEntity, currentTickTime + 1);
-        sendDummyPacket(itemEntity);
-    }
-
-    @Unique
-    private void sendDummyPacket(ItemEntity entity) {
-        ServerWorld world = (ServerWorld) entity.getWorld();
-        //? if >= 1.20.5 {
-        world.getPlayers(player -> player.canSee(entity))
-                .forEach(player -> {
-                    player.networkHandler.sendPacket(
-                            new CustomPayloadS2CPacket(
-                                    () -> DUMMY_SYNC_PACKET
-                            )
-                    );
-                });
-        //?} else if >= 1.19.3 && <= 1.20.4 {
-        /*PacketByteBuf buf = PacketByteBufs.create();
-        world.getPlayers(serverPlayerEntity -> serverPlayerEntity.canSee(entity))
-                .forEach(serverPlayerEntity -> {
-                    ServerPlayNetworking.send(serverPlayerEntity, DUMMY_SYNC_PACKET, buf);
-                });
-        *///?}
+            TICK_TIME_MAP.put(itemEntity, currentTickTime + 1);
+        }
     }
 }
--- src/main/java/banduty/ticktweaks/mixin/LivingEntityMixin.java
+++ src/main/java/banduty/ticktweaks/mixin/LivingEntityMixin.java
@@ -4,19 +4,11 @@ import banduty.ticktweaks.TickTweaks;
 import banduty.ticktweaks.configs.ModConfigs;
 import banduty.ticktweaks.util.TickHandlerUtil;
 import banduty.ticktweaks.util.TickRateCalculator;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.PacketByteBuf;
-//? if >= 1.20.5 {
-    import net.minecraft.network.packet.CustomPayload;
-    import net.minecraft.network.packet.s2c.common.CustomPayloadS2CPacket;
-//?}
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -41,13 +33,6 @@ public class LivingEntityMixin {
     private static final Map<LivingEntity, Integer> WAKEUP_INTERVAL_MAP = Collections.synchronizedMap(new WeakHashMap<>());
     @Unique
     private ModConfigs.PerformanceSettings.CustomActivationRange customActivationRange;
-    //? if >= 1.20.5 {
-    @Unique
-    private static final CustomPayload.Id<? extends CustomPayload> DUMMY_SYNC_PACKET = new CustomPayload.Id<>(Identifier.of(TickTweaks.MOD_ID, "dummy_sync"));
-     //?} else if >= 1.19.3 && <= 1.20.4 {
-    /*@Unique
-    private static final Identifier DUMMY_SYNC_PACKET = new Identifier(TickTweaks.MOD_ID, "dummy_sync");
-    *///?}
 
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     private void onTickEntity(CallbackInfo ci) {
@@ -67,7 +52,6 @@ public class LivingEntityMixin {
                 if (currentTickTime < tickInt) {
                     TICK_TIME_MAP.put(livingEntity, currentTickTime + 1);
                     ci.cancel();
-                    sendDummyPacket(livingEntity);
                     return;
                 }
             }
@@ -80,7 +64,6 @@ public class LivingEntityMixin {
                 if (currentTickTime < tickInt) {
                     TICK_TIME_MAP.put(livingEntity, currentTickTime + 1);
                     ci.cancel();
-                    sendDummyPacket(livingEntity);
                     return;
                 }
             }
@@ -92,7 +75,6 @@ public class LivingEntityMixin {
         if (activationSettings.isEnabled()) {
             handleCustomActivationWithCache(serverWorld, ci, currentTickTime);
             if (ci.isCancelled()) {
-                sendDummyPacket(livingEntity);
                 return;
             }
         }
@@ -112,13 +94,11 @@ public class LivingEntityMixin {
             boolean shouldCancel = TickHandlerUtil.tickCancellation(server, ci, withinRadius, tickRate, currentTickTime, defaultRange.getTickInterval());
             if (shouldCancel) {
                 TICK_TIME_MAP.put(livingEntity, 0);
-                sendDummyPacket(livingEntity);
                 return;
             }
         }
 
         TICK_TIME_MAP.put(livingEntity, currentTickTime + 1);
-        sendDummyPacket(livingEntity);
     }
 
     @Unique
@@ -158,26 +138,5 @@ public class LivingEntityMixin {
                     Collections.emptyList()
             );
         });
-    }
-
-    @Unique
-    private void sendDummyPacket(LivingEntity entity) {
-        ServerWorld world = (ServerWorld) entity.getWorld();
-        //? if >= 1.20.5 {
-        world.getPlayers(player -> player.canSee(entity))
-                .forEach(player -> {
-                    player.networkHandler.sendPacket(
-                            new CustomPayloadS2CPacket(
-                                    () -> DUMMY_SYNC_PACKET
-                            )
-                    );
-                });
-        //?} else if >= 1.19.3 && <= 1.20.4 {
-        /*PacketByteBuf buf = PacketByteBufs.create();
-        world.getPlayers(serverPlayerEntity -> serverPlayerEntity.canSee(entity))
-                .forEach(serverPlayerEntity -> {
-                    ServerPlayNetworking.send(serverPlayerEntity, DUMMY_SYNC_PACKET, buf);
-                });
-        *///?}
     }
 }
